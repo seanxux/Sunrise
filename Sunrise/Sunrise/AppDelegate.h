62914a3cf3ef753303c6a74883dfc8d906c29cb9
Xu Shawn <xiaotengxu@TMacbook.local>
2012-07-08 00:37:09 +0800

    Initial create project

---
+++ Sunrise/Sunrise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sunrise
+//
+//  Created by Shawn Xu on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sunrise/Sunrise/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sunrise
+//
+//  Created by Shawn Xu on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
